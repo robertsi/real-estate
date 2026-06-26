dba295a49c2cea8e305c23f2f63bd6431ef8c564
robertsi <robertsi@users.noreply.github.com>
2026-06-26 19:12:29 +0200

    some beach data

--- packages/shop/feature-map/src/lib/feature-map/feature-map.ts
+++ packages/shop/feature-map/src/lib/feature-map/feature-map.ts
@@ -7,6 +7,8 @@ import {
   PLATFORM_ID,
 } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { PLACES } from './map-data';
+import { SI_BEACHES } from './map-data-os-si';
 
 @Component({
   selector: 'shop-feature-map',
@@ -24,22 +26,39 @@ export class FeatureMap implements AfterViewInit {
 
     const L = await import('leaflet');
 
-    const loc: L.LatLngExpression = [46.5477847, 15.6243754];
+    const loc: L.LatLngExpression = [45.530751481732494, 13.564560421878287];
+    //const loc: L.LatLngExpression = [46.5477847, 15.6243754];
     //containes nepremicnine.net api key lol....
     //https://maps.googleapis.com/maps/api/geocode/json?language=&address=Ul.+Roberta+Hvalca+14%2C+2000+Maribor%2C+Slovenia&region=si&bounds=&components=&key=AIzaSyBilazss5s7dfNex93L6j6kRqF6kQCwhIo&sensor=false
     //https://nominatim.openstreetmap.org/search?q=Ulica+Roberta+Hvalca+14+Maribor&format=json
-    const map = L.map(this.mapEl.nativeElement).setView(
-      loc,
-      18,
-    );
+    const map = L.map(this.mapEl.nativeElement).setView(loc, 18);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors',
     }).addTo(map);
 
-    L.marker(loc)
-      .addTo(map)
-      .bindPopup('10, Ulica Roberta Hvalca, Maribor')
-      .openPopup();
+    L.marker(loc).addTo(map).bindPopup('10, Search location').openPopup();
+
+    // PLACES.forEach((place) => {
+    //   L.marker([place.location.latitude, place.location.longitude])
+    //     .addTo(map)
+    //     .bindPopup(place.displayName.text);
+    // });
+
+    // SI_BEACHES.forEach((place) => {
+    //   if(place.lat && place.lon && place.tags.name) {
+    //     L.marker([place.lat, place.lon])
+    //       .addTo(map)
+    //       .bindPopup(place.tags.name);
+    //   }
+    // });
+
+    SI_BEACHES.forEach((place) => {
+      if(place.center) {
+        L.marker([place.center.lat, place.center.lon])
+          .addTo(map)
+          .bindPopup("beach");
+      }
+    });
   }
 }
---
+++ packages/shop/feature-map/src/lib/feature-map/map-data.ts
@@ -0,0 +1,184 @@
+export const PLACES = [
+        {
+            "location": {
+                "latitude": 45.5349171,
+                "longitude": 13.66590985
+            },
+            "displayName": {
+                "text": "Slovene Riviera",
+                "languageCode": "en"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.5305512,
+                "longitude": 13.5645366
+            },
+            "displayName": {
+                "text": "Plaža Piran",
+                "languageCode": "si"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.5305512,
+                "longitude": 13.5645366
+            },
+            "displayName": {
+                "text": "Plaža Piran",
+                "languageCode": "si"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.525225299999995,
+                "longitude": 13.5808526
+            },
+            "displayName": {
+                "text": "Plaža Fiesa",
+                "languageCode": "si"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.525225299999995,
+                "longitude": 13.5808526
+            },
+            "displayName": {
+                "text": "Plaža Fiesa",
+                "languageCode": "si"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.5152384,
+                "longitude": 13.574714
+            },
+            "displayName": {
+                "text": "Bernardin Plaža",
+                "languageCode": "sl"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.5089835,
+                "longitude": 13.5937741
+            },
+            "displayName": {
+                "text": "Plaža Portorož",
+                "languageCode": "si"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.529827399999995,
+                "longitude": 13.602047299999999
+            },
+            "displayName": {
+                "text": "Plaža Strunjan",
+                "languageCode": "sl"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.529827399999995,
+                "longitude": 13.602047299999999
+            },
+            "displayName": {
+                "text": "Plaža Strunjan",
+                "languageCode": "sl"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.5089835,
+                "longitude": 13.5937741
+            },
+            "displayName": {
+                "text": "Plaža Portorož",
+                "languageCode": "si"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.5336218,
+                "longitude": 13.6299259
+            },
+            "displayName": {
+                "text": "Bele Skale beach",
+                "languageCode": "en"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.4830769,
+                "longitude": 13.5702669
+            },
+            "displayName": {
+                "text": "Kanegra Beach",
+                "languageCode": "en"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.5321715,
+                "longitude": 13.645397299999999
+            },
+            "displayName": {
+                "text": "Plaža Simonov zaliv",
+                "languageCode": "sl"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.5321715,
+                "longitude": 13.645397299999999
+            },
+            "displayName": {
+                "text": "Plaža Simonov zaliv",
+                "languageCode": "sl"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.541616,
+                "longitude": 13.656300799999999
+            },
+            "displayName": {
+                "text": "Plaža Izola",
+                "languageCode": "sl"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.5408773,
+                "longitude": 13.656317099999999
+            },
+            "displayName": {
+                "text": "Beach promenade",
+                "languageCode": "cs"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.541616,
+                "longitude": 13.656300799999999
+            },
+            "displayName": {
+                "text": "Plaža Izola",
+                "languageCode": "sl"
+            }
+        },
+        {
+            "location": {
+                "latitude": 45.5419397,
+                "longitude": 13.6592839
+            },
+            "displayName": {
+                "text": "Plaža slepih in slabovidnih",
+                "languageCode": "en"
+            }
+        }
+    ]
+
+    
